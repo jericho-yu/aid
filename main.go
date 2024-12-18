179e5fb7ecdfb7f3ea4c664c06ab53bca827625b
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2024-12-18 14:20:42 +0800

    [test]测试反射包通过名称调用方法

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ type Os struct{}
 
 func (*Os) TableName() string { return "t_os" }
 func main() {
-	val := reflection.New(Os{}).CallMethodByName("TableName")
+	val := reflection.New(&Os{}).CallMethodByName("TableName")
 	log.Printf("%#v\n", val[0].String())
 }
