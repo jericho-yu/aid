cd4297a0bc58b9e27b3027ccde628ea18c067a7b
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-01-14 10:23:01 +0800

    [optimize]优化publish.sh 支持 tag

--- publish.sh
+++ publish.sh
@@ -1,1 +1,11 @@
-git add --all && git commit -m"$1" && git push origin $2 && git checkout master && git merge $2 && git push origin master && git checkout $2 && git tag $3 && git push origin $3
+git add --all && \
+git commit -m"$1" && \
+git push origin $2 && \
+git checkout master && \
+git merge $2 && \
+git push origin master && \
+git checkout $2
+
+if [ -n "$3" ]; then
+	git tag "$3" && git push origin $3
+fi
