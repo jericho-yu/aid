8d5b60756bd3949a271c8f22eef8f5370e271f4c
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-02-07 12:01:50 +0800

    [optimize]

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
 git add --all && \
-git commit -m"$1" && \
-git push origin $2 && \
-git checkout master && \
-git merge $2 && \
-git push origin master && \
-git checkout $2
+git commit -m"$1"
+
+if [ -n "$2" ]; then 
+    git push && \
+	git checkout master && \
+	git merge $2 && \
+	git push && \
+	git checkout $2
+fi
 
 if [ -n "$3" ]; then
 	git tag "$3" && git push origin $3
