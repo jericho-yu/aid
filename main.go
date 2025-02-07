a0aa8193332012c1892709eb02549c963a4f6113
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-02-07 10:32:14 +0800

    [optimize]新增mongo连接池

--- main.go
+++ main.go
@@ -1,12 +1,110 @@
 package main
 
 import (
-	"github.com/jericho-yu/aid/str"
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Os struct{}
-
-func (*Os) TableName() string { return "t_os" }
 func main() {
-	str.NewTerminalLog("哈哈", "%s").Info("呵呵")
+	// 设置客户端选项
+	clientOptions := options.Client().ApplyURI("mongodb://admin:admin@localhost:27017")
+
+	// 连接到 MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 检查连接
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("成功连接到 MongoDB!")
+
+	// 使用特定的数据库和集合
+	collection := client.Database("test_db").Collection("test_collection")
+
+	// 插入新数据
+	newData := bson.D{
+		{Key: "name", Value: "Alice"},
+		{Key: "age", Value: 25},
+		{Key: "city", Value: "New York"},
+	}
+
+	insertResult, err := collection.InsertOne(context.TODO(), newData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("插入数据的ID:", insertResult.InsertedID)
+
+	// 根据ID搜索数据
+	var result bson.M
+	id := insertResult.InsertedID.(primitive.ObjectID)
+	if err = collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&result); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("OK: %v\n", result)
+
+	// 搜索多条信息
+	filter := bson.D{{Key: "age", Value: bson.D{{Key: "$gt", Value: 20}}}} // 搜索年龄大于20的记录
+	findOptions := options.Find().SetLimit(5)                              // 设置返回记录的最大数量
+	cursor, err := collection.Find(context.TODO(), filter, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(context.TODO())
+
+	var results []bson.M
+	cursor.All(context.TODO(), &results)
+	// for cursor.Next(context.TODO()) {
+	// 	var elem bson.M
+	// 	err := cursor.Decode(&elem)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	results = append(results, elem)
+	// }
+
+	// if err := cursor.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fmt.Println("搜索到的记录:")
+	for _, result := range results {
+		fmt.Println(result)
+	}
+
+	// 删除单条记录
+	deleteResult, err := collection.DeleteOne(context.TODO(), bson.M{"name": "Alice"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("删除了 %v 条记录\n", deleteResult.DeletedCount)
+
+	// 删除多条记录
+	deleteManyResult, err := collection.DeleteMany(context.TODO(), bson.M{"age": bson.M{"$gt": 20}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("删除了 %v 条记录\n", deleteManyResult.DeletedCount)
+
+	// 断开与 MongoDB 的连接
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("已断开与 MongoDB 的连接。")
 }
---
+++ mongoPool/mongo_pool.go
@@ -0,0 +1,104 @@
+package mongoPool
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/jericho-yu/aid/dict"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type (
+	MongoClientPool struct {
+		clients *dict.AnyDict[string, *MongoClient]
+	}
+
+	MongoClient struct {
+		url    string
+		client *mongo.Client
+	}
+)
+
+var (
+	mongoClientPool *MongoClientPool
+	mongoPoolOnce   sync.Once
+)
+
+// NewMongoClient 实例化：mongo客户端
+func NewMongoClient(url string) (*MongoClient, error) {
+	var err error
+	mc := &MongoClient{url: url}
+	// 设置客户端选项
+	clientOptions := options.Client().ApplyURI(mc.url)
+
+	// 连接到 MongoDB
+	if mc.client, err = mongo.Connect(context.TODO(), clientOptions); err != nil {
+		return nil, err
+	}
+
+	// 检查连接
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err = mc.client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+	return mc, nil
+}
+
+// Close 关闭客户端
+func (my *MongoClient) Close() error {
+	return my.client.Disconnect(context.Background())
+}
+
+// GetClient 获取客户端链接
+func (my *MongoClient) GetClient() *mongo.Client {
+	return my.client
+}
+
+// OnceMongoPool 单例化：mongodb连接池
+func OnceMongoPool() *MongoClientPool {
+	mongoPoolOnce.Do(func() {
+		mongoClientPool = &MongoClientPool{clients: dict.MakeAnyDict[string, *MongoClient]()}
+	})
+	return mongoClientPool
+}
+
+// Append 增加客户端
+func (*MongoClientPool) Append(key string, mongoClient *MongoClient) (*MongoClientPool, error) {
+	if mongoClientPool.clients.Has(key) {
+		return mongoClientPool, errors.New("客户端已存在")
+	}
+
+	mongoClientPool.clients.Set(key, mongoClient)
+
+	return mongoClientPool, nil
+}
+
+// 清除客户端
+func (*MongoClientPool) Remove(key string) (*MongoClientPool, error) {
+	if !mongoClientPool.clients.Has(key) {
+		return mongoClientPool, errors.New("客户端不存在")
+	}
+
+	mongoClient, exist := mongoClientPool.clients.Get(key)
+	if !exist {
+		return mongoClientPool, errors.New("客户端不存在")
+	}
+
+	if err := mongoClient.Close(); err != nil {
+		return mongoClientPool, err
+	}
+
+	return mongoClientPool, nil
+}
+
+// Clean 清理客户端
+func (*MongoClientPool) Clean() *MongoClientPool {
+	for _, key := range mongoClientPool.clients.Keys() {
+		mongoClientPool.Remove(key)
+	}
+	return mongoClientPool
+}
