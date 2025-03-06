f1fcd6ada66ec3ec1c5e70de12a907ff15ba5752
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-06 17:27:12 +0800

    [optimize]优化git.sh:更新:的用法为>空或.更加明确其含义

--- git.sh
+++ git.sh
@@ -10,11 +10,11 @@ elif [ "$operator" = "help" ]; then
 	echo "\t2、子程序名称为push-tag：创建并推送当前分支内容到tag（参数1表示tag名称）"
 	echo "\t3、子程序名称为last-tag：获取当前最后一个tag名称"
 	echo "\t4、其他子程序名称：分为三种情况"
-	echo "\t\t4.1、「source git.sh : 提交说明」：代表当前分支内容只提交不推送"
-	echo "\t\t4.2、「source git.sh dev: 提交说明」：代表当前内容推送到dev分支"
-	echo "\t\t4.3、「source git.sh dev:master 提交说明」：代表先推送到dev分支然后合并到master再推送到master"
+	echo "\t\t4.1、「source git.sh . 提交说明」：代表当前分支内容只提交不推送"
+	echo "\t\t4.2、「source git.sh dev 提交说明」：代表当前内容推送到dev分支"
+	echo "\t\t4.3、「source git.sh dev>master 提交说明」：代表先推送到dev分支然后合并到master再推送到master"
 else
-	IFS=':' read -r src_branch dst_branch <<<"$operator"
+	IFS='>' read -r src_branch dst_branch <<<"$operator"
 	commit="$2"
 	tag="$3"
 
@@ -24,11 +24,11 @@ else
 		git push origin $src_branch
 
 		if [ -n "$dst_branch" ]; then
-		git checkout $dst_branch &&
-			git merge $src_branch &&
-			git push origin $dst_branch &&
-			git checkout $src_branch
-	fi
+			git checkout $dst_branch &&
+				git merge $src_branch &&
+				git push origin $dst_branch &&
+				git checkout $src_branch
+		fi
 	fi
 
 	if [ -n "$tag" ]; then
