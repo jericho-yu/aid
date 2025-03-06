469fd9c92d31777632f3fba42ef9e65705850b4b
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-06 16:26:02 +0800

    [optimize]优化git.sh

---
+++ git.sh
@@ -0,0 +1,27 @@
+operator="$1"
+
+if [ "$operator" = "push-tag" ]; then
+	tag="$2"
+	git tag "$tag" && git push origin "$tag"
+elif [ "$operator" = "last-tag" ]; then
+	git describe --tags $(git rev-list --tags --max-count=1)
+else
+	commit="$2"
+	branch="$3"
+	tag="$4"
+
+	git add --all &&
+		git commit -m"$commit"
+
+	if [ -n "$branch" ]; then
+		git push origin $branch &&
+			git checkout $operator &&
+			git merge $branch &&
+			git push origin $operator &&
+			git checkout $branch
+	fi
+
+	if [ -n "$tag" ]; then
+		git tag "$tag" && git push origin "$tag"
+	fi
+fi
