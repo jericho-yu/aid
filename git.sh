b579f5fb09c997061b747d4e6a7ebf4a31871c2e
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-06 16:34:28 +0800

    [optimize]优化git.sh:支持->语法

--- git.sh
+++ git.sh
@@ -6,18 +6,18 @@ if [ "$operator" = "push-tag" ]; then
 elif [ "$operator" = "last-tag" ]; then
 	git describe --tags $(git rev-list --tags --max-count=1)
 else
-	branch="$2"
-	commit="$3"
-	tag="$4"
+	IFS='->' read -r branch target <<< "$operator"
+	commit="$2"
+	tag="$3"
 
 	git add --all &&
 		git commit -m"$commit"
 
 	if [ -n "$branch" ]; then
 		git push origin $branch &&
-			git checkout $operator &&
+			git checkout $target &&
 			git merge $branch &&
-			git push origin $operator &&
+			git push origin $target &&
 			git checkout $branch
 	fi
 
