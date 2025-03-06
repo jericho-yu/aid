b085231be3250521581e1154246c937340750663
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-06 16:52:37 +0800

    [operation]优化git.sh:支持分别推送src分支或src合并dst然后推送

--- git.sh
+++ git.sh
@@ -6,19 +6,21 @@ if [ "$operator" = "push-tag" ]; then
 elif [ "$operator" = "last-tag" ]; then
 	git describe --tags $(git rev-list --tags --max-count=1)
 else
-	IFS=':' read -r branch target <<< "$operator"
+	IFS=':' read -r src_branch dst_branch <<<"$operator"
 	commit="$2"
 	tag="$3"
 
-	git add --all &&
-		git commit -m"$commit"
+	git add --all && git commit -m"$commit"
 
-	if [ -n "$branch" ]; then
-		git push origin $branch &&
-			git checkout $target &&
-			git merge $branch &&
-			git push origin $target &&
-			git checkout $branch
+	if [ -n "$src_branch" ]; then
+		git push origin $src_branch
+	fi
+
+	if [ -n "$dst_branch" ]; then
+		git checkout $dst_branch &&
+			git merge $src_branch &&
+			git push origin $dst_branch &&
+			git checkout $src_branch
 	fi
 
 	if [ -n "$tag" ]; then
