865ea74fcd243cb4c13c2a3225e9d0503a5a037b
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-06 17:05:01 +0800

    [optimize]优化git.sh:新增help说明描述

--- git.sh
+++ git.sh
@@ -5,6 +5,14 @@ if [ "$operator" = "push-tag" ]; then
 	git tag "$tag" && git push origin "$tag"
 elif [ "$operator" = "last-tag" ]; then
 	git describe --tags $(git rev-list --tags --max-count=1)
+elif [ "$operator" = "help" ]; then
+	echo "\t1、基本用法：source git.sh 执行子程序名称 [参数1, 参数2...]"
+	echo "\t2、子程序名称为push-tag：创建并推送当前分支内容到tag（参数1表示tag名称）"
+	echo "\t3、子程序名称为last-tag：获取当前最后一个tag名称"
+	echo "\t4、其他子程序名称：分为三种情况"
+	echo "\t\t4.1、「source git.sh : 提交说明」：代表当前分支内容只提交不推送"
+	echo "\t\t4.2、「source git.sh dev: 提交说明」：代表当前内容推送到dev分支"
+	echo "\t\t4.3、「source git.sh dev:master 提交说明」：代表先推送到dev分支然后合并到master再推送到master"
 else
 	IFS=':' read -r src_branch dst_branch <<<"$operator"
 	commit="$2"
@@ -14,13 +22,13 @@ else
 
 	if [ -n "$src_branch" ]; then
 		git push origin $src_branch
-	fi
 
-	if [ -n "$dst_branch" ]; then
+		if [ -n "$dst_branch" ]; then
 		git checkout $dst_branch &&
 			git merge $src_branch &&
 			git push origin $dst_branch &&
 			git checkout $src_branch
+	fi
 	fi
 
 	if [ -n "$tag" ]; then
