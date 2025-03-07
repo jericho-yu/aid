3eb58cf176e71faf974f2f9f8d24279adb369b7a
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-07 15:56:04 +0800

    [optimize]三元运算增加TernaryFuncReturn

--- operation/operation.go
+++ operation/operation.go
@@ -4,13 +4,17 @@ package operation
 func Ternary[V any](condition bool, T, F V) V {
 	if condition {
 		return T
-	} else {
-		return F
 	}
+	return F
 }
 
 // TernaryFunc 三元运算：通过回调函数
 func TernaryFunc[V any](condition func() bool, T V, F V) V { return Ternary(condition(), T, F) }
+
+// TernaryFunc 三元运算：返回值使用回调方法
+func TernaryFuncReturn[V any](condition bool, trueFn func() V, falseFn func() V) V {
+	return Ternary(condition, trueFn(), falseFn())
+}
 
 // TernaryFuncAll 三元运算：通过回调函数，返回值也使用回调函数
 func TernaryFuncAll[V any](condition func() bool, trueFn func() V, falseFn func() V) V {
