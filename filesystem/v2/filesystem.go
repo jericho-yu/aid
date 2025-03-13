76ac477d9237b446a08a396abcbff5b9c3cdeb15
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-13 16:27:10 +0800

    [optimize]新增filesystem/v2.CopyFiles

--- filesystem/v2/filesystem.go
+++ filesystem/v2/filesystem.go
@@ -1,9 +1,17 @@
 package v2
 
-import "path/filepath"
+import (
+	"path/filepath"
+
+	"github.com/jericho-yu/aid/array"
+)
 
 func getRootPath(dir string) string {
 	rootPath, _ := filepath.Abs(".")
 
 	return filepath.Clean(filepath.Join(rootPath, dir))
+}
+
+func CopyFiles(srcFiles, dstFiles *array.AnyArray[*File]) {
+	srcFiles.Each(func(idx int, item *File) { item.CopyTo(dstFiles.Get(idx).GetFullPath()) })
 }
