ecbc0e85a3279478161207adbf3545512f6605e0
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-03-17 14:23:56 +0800

    [optimize]优化httpClientDownload代码

--- httpClient/http_client_download.go
+++ httpClient/http_client_download.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/jericho-yu/aid/array"
+	"github.com/jericho-yu/aid/dict"
 	processBar "github.com/schollz/progressbar/v3"
 )
 
@@ -72,13 +74,11 @@ func (my *HttpClientDownload) SendResponse(w http.ResponseWriter, headers ...map
 		w.Header().Set("Content-Disposition", "attachment; filename="+my.filename)
 		w.Header().Set("Content-Type", my.httpClient.response.Header.Get("Content-Type"))
 
-		for _, header := range headers {
-			for key, values := range header {
-				for _, value := range values {
-					w.Header().Set(key, value)
-				}
-			}
-		}
+		array.New(headers).Each(func(_ int, header map[string][]string) {
+			dict.New(header).Each(func(key string, values []string) {
+				array.New(values).Each(func(_ int, value string) { w.Header().Set(key, value) })
+			})
+		})
 
 		if _, my.httpClient.Err = io.Copy(w, my.httpClient.response.Body); my.httpClient.Err != nil {
 			my.httpClient.Err = WriteResponseErr.Wrap(my.httpClient.Err)
