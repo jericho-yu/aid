0b7aa757adcbf7fae522f75fc8835cd019d798d3
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-04-22 16:49:16 +0800

    [fix bug]修复table别名错误

--- gormPool/db_util.go
+++ gormPool/db_util.go
@@ -180,7 +180,7 @@ func (my *Finder) queryAutoFill(queries ...*FinderAutoQuery) error {
 		for _, query := range queries {
 			switch query.Operator {
 			case "alias":
-				my.DB.Table("%s as %s", query.Field, query.Values[0])
+				my.DB.Table(fmt.Sprintf("%s as %s", query.Field, query.Values[0]))
 			case "=", ">", "<", "!=", "<=", ">=":
 				my.DB.Where(fmt.Sprintf("%s %s ?", query.Field, query.Operator), query.Values[0])
 			case "in", "not in":
