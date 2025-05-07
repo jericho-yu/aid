a3a6d28105d722268cea183dab98449a860084b0
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-05-07 11:19:46 +0800

    [optimize]str增加ToPtr方法，获取字符串指针

--- str/buffer.go
+++ str/buffer.go
@@ -6,10 +6,13 @@ type Buffer struct{ original *bytes.Buffer }
 
 var BufferApp Buffer
 
+// NewByBuffer 实例化：通过字符串
 func (*Buffer) NewByString(original string) *Buffer { return &Buffer{bytes.NewBufferString(original)} }
 
+// NewByBytes 实例化：通过字节码
 func (*Buffer) NewByBytes(original []byte) *Buffer { return &Buffer{bytes.NewBuffer(original)} }
 
+// String 追加写入字符串
 func (my *Buffer) String(stringList ...string) *Buffer {
 	for _, s := range stringList {
 		my.original.WriteString(s)
@@ -18,6 +21,7 @@ func (my *Buffer) String(stringList ...string) *Buffer {
 	return my
 }
 
+// Byte 追加写入字节
 func (my *Buffer) Byte(byteList ...byte) *Buffer {
 	for _, b := range byteList {
 		my.original.WriteByte(b)
@@ -26,6 +30,7 @@ func (my *Buffer) Byte(byteList ...byte) *Buffer {
 	return my
 }
 
+// Rune 追加写入字符
 func (my *Buffer) Rune(runeList ...rune) *Buffer {
 	for _, v := range runeList {
 		my.original.WriteRune(v)
@@ -34,6 +39,14 @@ func (my *Buffer) Rune(runeList ...rune) *Buffer {
 	return my
 }
 
+// ToString 获取字符串
 func (my *Buffer) ToString() string { return my.original.String() }
 
+// ToBytes 获取字节码
 func (my *Buffer) ToBytes() []byte { return my.original.Bytes() }
+
+// ToPtr 获取字符串指针
+func (my *Buffer) ToPtr() *string {
+	ret := my.original.String()
+	return &ret
+}
