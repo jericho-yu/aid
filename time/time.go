bd0fd3314cf3ede332568e369382edf84ab7b9fb
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-05-21 16:15:26 +0800

    [feature]增加格式化常量

--- time/time.go
+++ time/time.go
@@ -8,7 +8,29 @@ import (
 // Time 时间帮助
 type Time struct{ original time.Time }
 
-var TimeApp Time
+var (
+	TimeApp Time
+
+	Layout      = time.Layout
+	ANSIC       = time.ANSIC
+	UnixDate    = time.UnixDate
+	RubyDate    = time.RubyDate
+	RFC822      = time.RFC822
+	RFC822Z     = time.RFC822Z
+	RFC850      = time.RFC850
+	RFC1123     = time.RFC1123
+	RFC1123Z    = time.RFC1123Z
+	RFC3339     = time.RFC3339
+	RFC3339Nano = time.RFC3339Nano
+	Kitchen     = time.Kitchen
+	Stamp       = time.Stamp
+	StampMilli  = time.StampMilli
+	StampMicro  = time.StampMicro
+	StampNano   = time.StampNano
+	DateTime    = time.DateTime
+	DateOnly    = time.DateOnly
+	TimeOnly    = time.TimeOnly
+)
 
 // New 实例化：时间帮助
 func (*Time) New(t time.Time) *Time { return &Time{original: t} }
