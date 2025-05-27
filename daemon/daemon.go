571b9378edddd884d0e3df7ce8d00f26955efa93
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-05-27 17:05:23 +0800

    [feature]守护进程去掉logger

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -9,47 +9,15 @@ import (
 	"time"
 
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-
-	"github.com/jericho-yu/aid/zapProvider"
 )
 
 // Daemon 守护进程服务提供者
-type Daemon struct {
-	logger *zap.Logger
-}
+type Daemon struct{}
 
 var App Daemon
 
 // New 实例化：
-func (*Daemon) New(logger *zap.Logger) *Daemon {
-	var (
-		err error
-		d   *Daemon
-	)
-
-	d = &Daemon{}
-
-	if logger == nil {
-		if d.logger, err = zapProvider.ZapProviderApp.New(
-			zapProvider.ZapProviderConfig.New(zapcore.ErrorLevel).
-				SetPath(".").
-				SetPathAbs(false).
-				SetInConsole(false).
-				SetEncoderType(zapProvider.EncoderTypeConsole).
-				SetNeedCompress(true).
-				SetMaxBackup(30).
-				SetMaxSize(10).
-				SetMaxDay(30),
-		); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		d.logger = logger
-	}
-
-	return d
-}
+func (*Daemon) New(logger *zap.Logger) *Daemon { return &Daemon{} }
 
 // Launch 启动守护进程
 func (my *Daemon) Launch(title, logDir string) {
@@ -66,7 +34,6 @@ func (my *Daemon) Launch(title, logDir string) {
 	logFilename := fmt.Sprintf("%s/runtime.log", logDir)
 	fp, err := os.OpenFile(logFilename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		my.logger.Error(title, zap.String("创建总日志失败", err.Error()))
 		log.Fatalf("【启动失败】创建总日志失败：%s", err.Error())
 	}
 	defer func() {
@@ -78,11 +45,9 @@ func (my *Daemon) Launch(title, logDir string) {
 	cmd.Stderr = fp
 	cmd.Stdin = nil
 	if err = cmd.Start(); err != nil {
-		my.logger.Error(title, zap.String("启动失败", err.Error()))
 		log.Fatalf("【启动失败】%s", err.Error())
 	}
 
-	my.logger.Info(title, zap.String("启动成功", ""), zap.Int("进程号", cmd.Process.Pid), zap.Time("启动时间", time.Now()))
 	fp.WriteString(fmt.Sprintf("(>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\r\n%s 程序启动成功 [进程号->%d] 启动于：%s\r\n", title, cmd.Process.Pid, time.Now().Format(string(time.DateTime+".000"))))
 
 	os.Exit(0)
