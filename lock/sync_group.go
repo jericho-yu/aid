32fcf47459549d7fab8f9eb0f88512df92723d7e
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-06-16 16:14:34 +0800

    [feature]增加lock.SyncGroup

---
+++ lock/sync_group.go
@@ -0,0 +1,17 @@
+package lock
+
+import "sync"
+
+type SyncGroup[T func(values ...any)] struct{ sw *sync.WaitGroup }
+
+// NEW 实例化
+func (SyncGroup[T]) NEW() *SyncGroup[T] { return &SyncGroup[T]{sw: &sync.WaitGroup{}} }
+
+func (my *SyncGroup[T]) Go(fn T) {
+	my.sw.Add(1)
+	go func() {
+		defer my.sw.Done()
+		fn()
+	}()
+	my.sw.Wait()
+}
