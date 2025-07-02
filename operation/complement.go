83dc256248a925b22220796ee7ca9787c7dd99de
jericho-yu <yu.jericho.ph@gmail.com>
2025-07-02 13:02:46 +0800

    [feature]新增取交、并、差集

--- operation/complement.go
+++ operation/complement.go
@@ -1,9 +1,53 @@
 package operation
 
-// 取两个数组补集
-func GetComplement[T any](setA, setB []T) []T {
-	complement := []T{}
-	elementMap := make(map[any]bool)
+// 获取两个数组的交集
+func GetIntersection[T comparable](setA, setB []T) []T {
+	intersection := []T{}
+	elementMap := make(map[T]bool)
+
+	// 将 setB 的元素存入 map
+	for _, b := range setB {
+		elementMap[b] = true
+	}
+
+	// 遍历 setA，检查是否在 setB 中
+	for _, a := range setA {
+		if elementMap[a] {
+			intersection = append(intersection, a)
+		}
+	}
+
+	return intersection
+}
+
+// 获取两个数组的并集
+func GetUnion[T comparable](setA, setB []T) []T {
+	union := []T{}
+	elementMap := make(map[T]bool)
+
+	// 将 setA 的元素存入 map
+	for _, a := range setA {
+		if !elementMap[a] {
+			union = append(union, a)
+			elementMap[a] = true
+		}
+	}
+
+	// 将 setB 的元素存入 map
+	for _, b := range setB {
+		if !elementMap[b] {
+			union = append(union, b)
+			elementMap[b] = true
+		}
+	}
+
+	return union
+}
+
+// 获取两个数组的差集
+func GetDifference[T comparable](setA, setB []T) []T {
+	difference := []T{}
+	elementMap := make(map[T]bool)
 
 	// 将 setB 的元素存入 map
 	for _, b := range setB {
@@ -13,9 +57,9 @@ func GetComplement[T any](setA, setB []T) []T {
 	// 遍历 setA，检查是否不在 setB 中
 	for _, a := range setA {
 		if !elementMap[a] {
-			complement = append(complement, a)
+			difference = append(difference, a)
 		}
 	}
 
-	return complement
+	return difference
 }
